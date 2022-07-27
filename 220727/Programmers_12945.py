3e6ace6688f587ddcf88148a6eeacdeb8ef7c065
aeriheo <33409245+aeriheo@users.noreply.github.com>
2022-07-27 12:55:42 +0900

    [Programmers] 22.07.27

---
+++ 220727/Programmers_12945.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    f = [0] * 100001
+    f[0] = 0
+    f[1] = 1
+    for i in range(2, n + 1):
+        f[i] = f[i - 1] + f[i - 2]
+
+    answer = f[n] % 1234567
+    return answer
